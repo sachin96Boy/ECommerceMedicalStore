52686f95e4e8f9240161d63ed25ecafb872214da
sachin96Boy <sachinsupunthaka96@gmail.com>
2020-12-07 13:51:03 +0530

    add reatng function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import prodData from './data'
+import prodData from './data';
+import Product from './components/Product';
+
 
 
 function App() {
   return (
+
     <div className="grid-container">
 
       <header className="row">
@@ -24,37 +27,8 @@ function App() {
         <div className="row center">
           {
             prodData.products.map((product) => (
-              <div key={product._id} className="card">
-
-                <a href={'/product/product._id'}>
-                  <img className="medium" src={product.image} alt={product.name} />
-                </a>
-                <div className="card-body">
-                  <a href={'/product/product._id'}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">
-                    ${product.price}
-                    </div>
-                </div>
-              </div>),
+              <Product key={product._id} product = {product}> </Product>
+                  )
               )
           }
 
@@ -68,9 +42,10 @@ function App() {
 
       <footer className="row center">
 
-        Contact Us On  <strong className="footer-center">FACEBOOK</strong>
+        Contact Us On FACEBOOK
       </footer>
     </div>
+
 
   );
 }
