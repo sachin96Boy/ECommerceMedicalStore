b5264f78929d62e8c63b4935f33a94e46937f18d
sachin96Boy <sachinsupunthaka96@gmail.com>
2020-12-07 11:14:33 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,77 @@
+const prodData = {
+    products:[
+        {
+            _id: '1',
+            name: 'medical Supplies',
+            category: 'Medicine_things',
+            image: '/images/p_1.png',
+            price: '150',
+            brand: 'Moizzeralla',
+            rating: '4.5',
+            numReviews: 10,
+            description: 'High quality and important Medicine for you'
+        },
+
+        {
+            _id: '2',
+            name: 'medical Supplies',
+            category: 'Medicine_things',
+            image: '/images/p_1.png',
+            price: '150',
+            brand: 'Moizzeralla',
+            rating: '4.5',
+            numReviews: 10,
+            description: 'High quality and important Medicine for you'
+        },
+
+        {
+            _id: '3',
+            name: 'medical Supplies',
+            category: 'Medicine_things',
+            image: '/images/p_1.png',
+            price: '150',
+            brand: 'Moizzeralla',
+            rating: '4.5',
+            numReviews: 10,
+            description: 'High quality and important Medicine for you'
+        },
+
+        {
+            _id: '4',
+            name: 'medical Supplies',
+            category: 'Medicine_things',
+            image: '/images/p_1.png',
+            price: '150',
+            brand: 'Moizzeralla',
+            rating: '4.5',
+            numReviews: 10,
+            description: 'High quality and important Medicine for you'
+        },
+
+        {
+            _id: '5',
+            name: 'medical Supplies',
+            category: 'Medicine_things',
+            image: '/images/p_1.png',
+            price: '150',
+            brand: 'Moizzeralla',
+            rating: '4.5',
+            numReviews: 10,
+            description: 'High quality and important Medicine for you'
+        },
+
+        {
+            _id: '6',
+            name: 'medical Supplies',
+            category: 'Medicine_things',
+            image: '/images/p_1.png',
+            price: '150',
+            brand: 'Moizzeralla',
+            rating: '4.5',
+            numReviews: 10,
+            description: 'High quality and important Medicine for you'
+        }
+    ]
+}
+
+export default prodData;
